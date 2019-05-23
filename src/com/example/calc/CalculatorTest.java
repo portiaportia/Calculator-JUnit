bf1d1f8e608034170b07dc8a18cff3d11a4f158c
portiaportia <portiaplante@gmail.com>
2019-05-23 16:04:37 -0400

    Add more documentation

--- src/com/example/calc/CalculatorTest.java
+++ src/com/example/calc/CalculatorTest.java
@@ -2,10 +2,44 @@ package com.example.calc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
+	
+	@BeforeClass
+	public static void oneTimeSetup() {
+		
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		
+	}
+	
+	@Before
+	public static void setup() {
+		//runs before each test
+	}
+	
+	@After
+	public static void tearDown() {
+		//runs after each test
+	}
+	
+	//assertEquals(val1,val2)
+	//assertFalse(val)
+	//assertTrue(val)
+	//assertSame(val1,val2)
+	//assertNotSame(val1,val2)
+	//assertNull(val)
+	//assertNotNull(val)
+	
+	
+	
 	
 	@Test
 	public void testFactorialValidNum() {
